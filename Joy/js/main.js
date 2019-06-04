4b37da4635eb7a5c8cdb21d4544f8aebef204de7
smokeSkiller <31475816+smokeSkiller@users.noreply.github.com>
2019-06-04 15:31:24 +0500

    dasd

---
+++ Joy/js/main.js
@@ -0,0 +1,27 @@
+//! DOM
+const mainSlider = $(".main-slider");
+const directionsSlider = $(".directions-slider");
+const gallerySlider = $(".gallery-slider");
+let sameSliders = [mainSlider, directionsSlider, gallerySlider];
+
+//! smooth scrolling
+$(function() {
+    // scrollSpeed(step, speed);
+    jQuery.scrollSpeed(100, 800);
+});
+
+//* Sliders
+sameSliders.forEach( function (slider) {
+    slider.slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        dotsClass: "slider-dots",
+        draggable: false,
+        prevArrow: '<button class="slick-prev"><img src="../img/ic_scroll-left.png" width="25px" height="48px"></button>',
+        nextArrow: '<button class="slick-next"><img src="../img/ic_scroll-right.png" width="25px" height="48px"></button>',
+        zIndex: 111,
+        responsive: true
+    });
+});
