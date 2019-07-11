64fcb94e24fe91b5ed81052038896cd50e406d56
smokeSkiller <31475816+smokeSkiller@users.noreply.github.com>
2019-07-11 13:29:14 +0500

    asd

---
+++ fucking-menu/js/common.js
@@ -0,0 +1,55 @@
+const dropdownLink = $(".has-child");
+const burgerBtn = $(".burger-btn");
+const menuItems = $(".menu-items");
+const menuArrow = $(".has-child .arrow");
+let dropdown, dropdownArrow;
+
+//! Nav menu while resizing
+$(window).on("resize", function () {
+    if ($(this).width() >= 992) {
+        burgerBtn.removeClass("on");
+        menuItems.css("display", "flex");
+        $(".dropdown").removeClass("active").css("display", "none");
+    } else {
+        burgerBtn.removeClass("on");
+        menuItems.css("display", "none");
+    }
+});
+
+//! Clicking to arrows 
+menuArrow.on("click", function () {
+    dropdownArrow = $(this).parent().parent().children(".dropdown");
+    dropdownArrow.slideToggle().addClass("active");
+    return false;
+});
+
+//! Burger btn
+burgerBtn.on("click", function () {
+    $(this).toggleClass("on");
+    menuItems.slideToggle();
+});
+
+//! Hover
+    dropdownLink.hover(function () {
+        if ($(window).width() >= 992) {        
+        dropdown = $(this).children(".dropdown");
+        dropdown.stop(false, true).slideDown().addClass("active");
+    
+        if ($(this).hasClass("has-child-1")) {
+            if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
+                dropdown.css("left", "-112px");
+            }
+        }
+    
+        if ($(this).hasClass("has-child-2")) {
+            if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
+                dropdown.css("left", "-245px");
+            }
+        }
+    }
+    
+    }, function () {
+        if ($(window).width() >= 992) {        
+        $(this).children(".dropdown").stop(false, true).slideUp().removeClass("active");
+        }
+    });
