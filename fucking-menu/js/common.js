a89eae187b89d8ef8b65db657cb930839ff236c5
smokeSkiller <31475816+smokeSkiller@users.noreply.github.com>
2019-07-11 18:03:31 +0500

    edawewe

--- fucking-menu/js/common.js
+++ fucking-menu/js/common.js
@@ -2,7 +2,24 @@ const dropdownLink = $(".has-child");
 const burgerBtn = $(".burger-btn");
 const menuItems = $(".menu-items");
 const menuArrow = $(".has-child .arrow");
+const arrowLink = $(".arrow-link");
+const topLine = $(".top-line");
+const header = $("header");
 let dropdown, dropdownArrow;
+
+//! Fixed nav
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > header.height() + 120) {
+        topLine.addClass("fixed");
+        menuItems.css("top", "60px");
+    } else {
+        topLine.removeClass("fixed");
+        menuItems.css("top", "0px");
+    }
+});
+
+//! Arrow link
+arrowLink.append('<div class="arrow"><i class="fas fa-angle-down"></i></div>');
 
 //! Nav menu while resizing
 $(window).on("resize", function () {
@@ -30,26 +47,26 @@ burgerBtn.on("click", function () {
 });
 
 //! Hover
-    dropdownLink.hover(function () {
-        if ($(window).width() >= 992) {        
-        dropdown = $(this).children(".dropdown");
-        dropdown.stop(false, true).slideDown().addClass("active");
-    
-        if ($(this).hasClass("has-child-1")) {
-            if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
-                dropdown.css("left", "-112px");
-            }
-        }
-    
-        if ($(this).hasClass("has-child-2")) {
-            if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
-                dropdown.css("left", "-245px");
-            }
+dropdownLink.hover(function () {
+    if ($(window).width() >= 992) {        
+    dropdown = $(this).children(".dropdown");
+    dropdown.stop(false, true).slideDown().addClass("active");
+
+    if ($(this).hasClass("has-child-1")) {
+        if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
+            dropdown.css("left", "-112px");
         }
     }
-    
-    }, function () {
-        if ($(window).width() >= 992) {        
-        $(this).children(".dropdown").stop(false, true).slideUp().removeClass("active");
+
+    if ($(this).hasClass("has-child-2")) {
+        if (dropdown.offset().left + dropdown.width() >= $(window).width()) {
+            dropdown.css("left", "-245px");
         }
-    });
+    }
+}
+
+}, function () {
+    if ($(window).width() >= 992) {        
+    $(this).children(".dropdown").stop(false, true).slideUp().removeClass("active");
+    }
+});
