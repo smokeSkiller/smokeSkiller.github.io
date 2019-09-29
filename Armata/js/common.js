ff8e45944ff8c85c0018dfae05f17bcef834e2eb
smokeSkiller <31475816+smokeSkiller@users.noreply.github.com>
2019-09-29 18:29:34 +0500

    4123

--- Armata/js/common.js
+++ Armata/js/common.js
@@ -11,8 +11,46 @@ $(function() {
 
 	//! Events
 	burgerBtn.on("click", function (event) {
+		event.stopPropagation();
 		$(event.currentTarget).toggleClass("on");
-		mainMenu.slideToggle("active");
+		mainMenu.slideToggle();
+	});
+
+	if ($(window).width() <= 1200) {
+		$("header .top-line .main-menu a").removeClass("active");
+	}
+
+	$(window).on("resize", function () {
+		if ($(window).width() <= 1200) {
+			$("header .top-line .main-menu a").removeClass("active");
+		} else {
+			$("header .top-line .main-menu a").eq(0).addClass("active");
+		}
+	});
+
+	$(document.body).on("click", function (params) {
+		if ($(window).width() <= 1200) {
+			burgerBtn.removeClass("on");
+			// header
+			mainMenu.slideUp(150);
+		} else {
+			mainMenu.slideDown(150);
+		}
+	});
+
+	mainMenu.on("click", function (e) {
+		e.stopPropagation();
+	});
+
+	//! Nav menu while resizing
+	$(window).on("resize", function () {
+		if ($(this).width() >= 1200) {
+			burgerBtn.removeClass("on");
+			mainMenu.css("display", "flex");
+		} else {
+			burgerBtn.removeClass("on");
+			mainMenu.css("display", "none");
+		}
 	});
 
 	// Scroll down button
@@ -25,7 +63,6 @@ $(function() {
 	scrollUpBtn.click(function() {
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 	});
-
 
 	//! Slider
 	commentSlider.slick({
