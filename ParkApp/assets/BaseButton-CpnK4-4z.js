fc22bf34d8ebbeb55566161e62fcddfa181d502e
Timur Sharipov <sharipow03@mail.ru>
2025-10-13 03:45:45 +0500

    Fix

---
+++ ParkApp/assets/BaseButton-CpnK4-4z.js
@@ -0,0 +1,1 @@
+import{_ as t,c as n,o,j as s,a}from"./index-C4VnoLHs.js";const c={},r={class:"btn-text"};function d(e,_){return o(),n("button",null,[s(e.$slots,"icon",{},void 0),a("span",r,[s(e.$slots,"default",{},void 0)])])}const f=t(c,[["render",d],["__scopeId","data-v-1f18475d"]]);export{f as B};
